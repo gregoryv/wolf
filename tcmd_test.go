209f45d53629979d6df839e0c9a2d2e9286f72e3
Gregory Vincic <g@7de.se>
2021-04-30 07:56:22 +0200

    Simplify tests

--- tcmd_test.go
+++ tcmd_test.go
@@ -3,27 +3,23 @@ package wolf
 import (
 	"io"
 	"testing"
-
-	"github.com/gregoryv/asserter"
 )
 
-func TestNewTestCmd(t *testing.T) {
-	cmd := NewTCmd()
-	defer cmd.Cleanup()
+func TestNewTCmd(t *testing.T) {
+	cases := map[string]*TCmd{
+		"no arguments":       NewTCmd(),
+		"single argument":    NewTCmd("-h"),
+		"multiple arguments": NewTCmd("-p", "-v"),
+	}
 
-	assert := asserter.Wrap(t).Assert
-	assert(cmd.Getenv("PWD") != "").Error(`cmd.Getenv("PWD") failed `)
-	assert(len(cmd.Args()) != 0).Error("empty cmd.Args")
-	wd, _ := cmd.Getwd()
-	assert(wd != "").Error("empty cmd.Getwd()")
-	assert(cmd.Stdin() != nil).Error("nil cmd.Stdin")
-	assert(cmd.Stdout() != nil).Error("nil cmd.Stdout")
-	assert(cmd.Stderr() != nil).Error("nil cmd.Stderr")
-	cmd.Exit(1)
-	assert(cmd.ExitCode == 1).Errorf("ExitCode not 1")
-
-	cmd.Stdout().Write([]byte("line1\nline2\nline 3"))
-	cmd.Stderr().Write([]byte("line1\nline2\nline 3"))
+	for name, cmd := range cases {
+		t.Run(name, func(t *testing.T) {
+			if cmd == nil {
+				t.FailNow()
+			}
+			cmd.Cleanup()
+		})
+	}
 }
 
 func Test_handleErr(t *testing.T) {
